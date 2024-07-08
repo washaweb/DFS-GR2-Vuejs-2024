81db52d2d080eae222ba9e044e6ac40639626239
Jérôme Poslednik <webmaster@washaweb.com>
2024-07-08 14:40:13 +0200

    correction todo avec boutons

--- TP/03-events/js/app-corrige.js
+++ TP/03-events/js/app-corrige.js
@@ -9,6 +9,7 @@ createApp({
           id: 1,
           text: 'Titre de la todo',
           done: false,
+          edit: true,
         },
         {
           id: 2,
@@ -32,6 +33,32 @@ createApp({
         },
       ],
       debug: false
+    }
+  },
+  methods: {
+    // ici on réaffecte la valeur du tableau avec un filtre qui supprime la todo recherchée
+    onDelete(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    // ici on modifie la valeur de la todo recherche pour passer todo.edit à true, ce qui va afficher le template d'édition correspondant dans la vue
+    onEdit(id) {
+      const todo = this.todos.find(todo => todo.id === id)
+      todo.edit = true
+      todo.form = todo.text
+    },
+    // ici on annule la modification en remettant les valeurs à 0
+    onCancel(id){
+      const todo = this.todos.find(todo => todo.id === id)
+      todo.form = ''
+      todo.edit = false
+    },
+
+    // ici on recherche dans le tableau la todo à modifier, on récupère la valeur du form pour l'insérer dans le texte, puis on remet le edit à false et vidange le form pour pouvoir enregistrer une nouvelle valeur
+    onSave(todo) {
+      const todoEdited = this.todos.find(t => t.id === todo.id)
+      todoEdited.text = todoEdited.form
+      todoEdited.form = ''
+      todoEdited.edit = false
     }
   }
 }).mount('#app')
