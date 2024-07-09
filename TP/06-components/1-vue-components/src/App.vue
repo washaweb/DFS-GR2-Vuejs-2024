f5f1dcd74f81688ae180de0510c21e23c4ba2d0c
Jérôme Poslednik <webmaster@washaweb.com>
2024-07-09 10:02:53 +0200

    composants, sass, bootstrap et fontawesome avec npm

---
+++ TP/06-components/1-vue-components/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hello vue <i class="fa-regular fa-envelope"></i></h1>
+  <div class="d-flex gap-2">
+    <button class="btn btn-primary">Exemple de bouton</button>
+    <button class="btn btn-secondary">Exemple de bouton</button>
+    <button class="btn btn-success">Exemple de bouton</button>
+    <button class="btn btn-danger">Exemple de bouton</button>
+    <button class="btn btn-warning">Exemple de bouton</button>
+    <button class="btn btn-info">Exemple de bouton</button>
+  </div>
+</template>
