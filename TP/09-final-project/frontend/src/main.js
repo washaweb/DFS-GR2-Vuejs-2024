7c534cf245354a41a1d55e6edd5ca05dfa9937eb
Jérôme Poslednik <webmaster@washaweb.com>
2024-07-12 11:47:23 +0200

    ajout de axios dans le frontend

--- TP/09-final-project/frontend/src/main.js
+++ TP/09-final-project/frontend/src/main.js
@@ -1,14 +1,32 @@
 import './assets/main.scss'
-
-import { createPinia } from 'pinia'
+// import de vue createApp
 import { createApp } from 'vue'
 
+//import de pinia
+import { createPinia } from 'pinia'
+
+// import du plugin axios
+import axiosPlugin from '@/plugins/axios.js'
+
+// import du composant de base
 import App from './App.vue'
+
+// import du router
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+// on déclare le plugin axios avant pinia
+app.use(axiosPlugin)
+// on déclare une instance de pinia
+const pinia = createPinia()
+// on configure pinia pour avoir notre instance d'axios accessible dans les stores
+pinia.use(({ store }) => {
+  // ici on injecte l'instance de axios dans le store de pinia
+  store.$http = app.config.globalProperties.$http
+})
+// puis on déclare pinia comme dépendance de notre app
+app.use(pinia)
+// enfin le router...
 app.use(router)
 
 // enregistrement de composants globaux
