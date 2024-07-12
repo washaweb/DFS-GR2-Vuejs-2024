db62019747b61424affae8867f3a7868e9e2ab81
Jérôme Poslednik <webmaster@washaweb.com>
2024-07-12 09:11:23 +0200

    config serveur node

--- TP/09-final-project/backend/routes/bill.mjs
+++ TP/09-final-project/backend/routes/bill.mjs
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
+// ici le / correspond à la route /bills/ (voir le index.mjs qui déclare l'import de ces routes avec app.use('/bills, billRoutes)
 router.get('/', (req, res) => {
   res.send('Hello liste des Factures !')
 })
 
+// ici la route /bills/:id
 router.get('/:id', (req, res) => {
   res.send(`Hello facture ${ req.params.id } !`)
 })
